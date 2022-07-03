ceee3838861c7d1b089e059f2523c17f4366481b
sge0208 <geruijia0208@gmail.com>
2022-07-03 00:40:46 -0400

    add addnotes page

--- src/pages/AddNote.js
+++ src/pages/AddNote.js
@@ -1,5 +1,25 @@
-function AddNote () {
-    return <>MyComponent</>;
+import { FormControl, TextField, Button } from "@material-ui/core";
+
+function AddNote() {
+    return (
+        <form>
+            <FormControl fullWidth>
+                <TextField label="Title" variant="outlined" />
+            </FormControl>
+            <FormControl fullWidth>
+                <TextField label="Text" multiline rows={4} variant="outlined" />
+            </FormControl>
+            <div>
+                <Button type="button" color="secondary">
+                    Cancel
+                </Button>
+                <Button type="submit" color="primary">
+                    Submit
+                </Button>
+            </div>
+        </form>
+    );
 }
 
-export default MyComponent;
+export default AddNote;
+
--- src/pages/DisplayNotes.js
+++ src/pages/DisplayNotes.js
@@ -1,5 +1,30 @@
-function DisplayNotes () {
-    return <>MyComponent</>;
+import { List, Fab, withStyles } from "@material-ui/core";
+import { Add } from "@material-ui/icons";
+import Note from "../components/Note";
+
+const styles = {
+    fab: {
+        position: 'absolute',
+        bottom: "2rem",
+        right: "2rem",
+    }
+};
+
+function DisplayNotes(props) {
+    const { notes, deleteNote, classes } = props;
+    return (
+        <>
+            <List>
+                {notes.map((note, index) => {
+                    return <Note note={note} key={index} deleteNote={deleteNote} />;
+                })}
+            </List>
+            <Fab aria-label={"Add"} className={classes.fab}>
+                <Add />
+            </Fab>
+        </>
+    );
 }
 
-export default MyComponent;
+export default withStyles(styles)(DisplayNotes);
+
