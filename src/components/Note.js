3e0e9f98d4b384e4d0b40784645a71b9e94e907c
sge0208 <geruijia0208@gmail.com>
2022-07-02 23:28:53 -0400

    make notes collapsible

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,14 +1,39 @@
-import { ListItem, ListItemText } from "@material-ui/core";
+import React, { Component } from "react";
+import { Collapse, List, ListItem, ListItemText, ListItemIcon } from "@material-ui/core";
+import { ExpandLess, ExpandMore } from "@material-ui/icons";
 
-function Note(props) {
-    const { note } = props;
+class Note extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+        };
+    }
 
-    return (
-        <ListItem>
-            <ListItemText primary={note.title} secondary={note.text} />
-        </ListItem>
-    );
+    handleClick = () => {
+        this.setState({ open: !this.state.open });
+    };
+
+    render() {
+        const { note } = this.props;
+        const { open } = this.state;
+
+        return (
+            <>
+                <ListItem onClick={this.handleClick}>
+                    <ListItemIcon>
+                        { open ? <ExpandLess /> : <ExpandMore />}
+                    </ListItemIcon>
+                    <ListItemText primary={note.title} />
+                </ListItem>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <ListItemText secondary={note.text} />
+                    </List>
+                </Collapse>
+            </>
+        );
+    }
 }
 
 export default Note;
-
